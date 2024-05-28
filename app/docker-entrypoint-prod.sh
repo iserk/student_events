2315bd359ac3086be187705ecbd7eaa0f573caf7
iserk <igor.serkizyuk@gmail.com>
2024-05-28 03:38:48 +0300

    Updated the prod settings

--- app/docker-entrypoint-prod.sh
+++ app/docker-entrypoint-prod.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Verify that Postgres is healthy before applying the migrations and running the Django development server
-if [ "$DATABASE" = "postgres" ]; then echo "Waiting for postgres..."
-    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-      sleep 0.1
-    done
-    echo "PostgreSQL is accepting connections"
-fi
+# # Verify that Postgres is healthy before applying the migrations and running the Django development server
+# if [ "$DATABASE" = "postgres" ]; then echo "Waiting for postgres..."
+#     while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+#       sleep 0.1
+#     done
+#     echo "PostgreSQL is accepting connections"
+# fi
 
 echo "Apply database migrations"
 python manage.py migrate
 
 echo "Starting server"
-/usr/local/bin/gunicorn sams.wsgi:application --workers 2 --bind 0.0.0.0:8000
+/usr/local/bin/gunicorn sams.wsgi:application --workers 2 --bind :8000
