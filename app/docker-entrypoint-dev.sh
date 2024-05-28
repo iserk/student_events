6ebeb2ab23d41f407da3a2e2a7d7f0cdf5ccee5e
iserk <igor.serkizyuk@gmail.com>
2024-05-28 03:09:50 +0300

    Implemented multi-stage Dockerfile for the Django app

---
+++ app/docker-entrypoint-dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Verify that Postgres is healthy before applying the migrations and running the Django development server
+if [ "$DATABASE" = "postgres" ]; then echo "Waiting for postgres..."
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+    echo "PostgreSQL is accepting connections"
+fi
+
+echo "Apply database migrations"
+python manage.py migrate
+
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
